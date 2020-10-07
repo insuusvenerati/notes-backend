d603c43e27c6e6024a3a33a0f5466dd9aeec5bf4
Sean Norwood <norwood.sean@gmail.com>
2020-10-06 23:45:10 -0500

    chore: initial commit

---
+++ config/plugins.js
@@ -0,0 +1,7 @@
+module.exports = {
+  graphql: {
+    apolloServer: {
+      tracing: true,
+    },
+  },
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd686122a-80b5-4590-abb8-9f26a5853335'
+};
